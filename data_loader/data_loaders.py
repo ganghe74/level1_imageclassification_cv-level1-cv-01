53d1ca5a38a0253cbe368f8b31aa3cab084d467e
sangheon <qwer55252@gmail.com>
2022-10-27 07:26:24 +0000

    after first augmentaion train

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 filenames = ['incorrect_mask', 'mask1', 'mask2', 'mask3', 'mask4', 'mask5', 'normal']
 masklabels = [1, 0, 0, 0, 0, 0, 2]
-aug_masklabels = [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2]
+
 
 class MaskTrainDataset(Dataset):
     def __init__(self, root, transform, training=True):
@@ -16,11 +16,17 @@ class MaskTrainDataset(Dataset):
         self.is_train = training
         self.transform = transform
         
+        ### augmentations 달라지면 수정해줘야함
+        self.aug_name = '_CC'
+        self.aug_filenames = ['incorrect_mask'+self.aug_name, 'mask1_CC'+self.aug_name, 'mask2_CC'+self.aug_name, 'mask3_CC'+self.aug_name, 'mask4_CC'+self.aug_name, 'mask5_CC'+self.aug_name, 'normal_CC'+self.aug_name]
+        self.aug_masklabels = [1, 0, 0, 0, 0, 0, 2]
+        ###
+        
         self.paths = []
         if self.is_train:
             self.df = pd.read_csv(os.path.join(root, 'train', 'train.csv'))
             self.labels = []
-            for path in self.df['path']:
+            for path in self.df['path']: # path == 000002_female_Asian_52
                 _, gender, _, age = path.split('_')
                 age = int(age)
                 gender = 0 if gender == 'male' else 1
@@ -31,14 +37,17 @@ class MaskTrainDataset(Dataset):
                 else:
                     age = 2
 
-                for file, mask in zip(filenames, masklabels):
+                filenames = ['incorrect_mask', 'mask1', 'mask2', 'mask3', 'mask4', 'mask5', 'normal']
+                for file, mask in zip(filenames, masklabels): # file == "incorrect_mask", mask == 1
                     p = os.path.join(root, 'train', 'images', path, file+'*')
                     self.paths.extend(glob.glob(p))
                     self.labels.append(mask * 6 + gender * 3 + age)
-                    for aug_mask in aug_masklabels:
-                        aug_p = os.path.join(root, 'off_aug', 'images', path, file+'*')
-                        self.paths.extend(glob.glob(aug_p))
-                        self.labels.append(aug_mask * 6 + gender * 3 + age)
+                    
+                    # for file, aug_mask in zip(aug_filenames, aug_masklabels): # file == "incorrect_mask_CC"
+                    aug_p = os.path.join(root, 'off_aug', 'images', path, file+self.aug_name+'*') # _CC 변수로 바꿔주자
+                    self.paths.extend(glob.glob(aug_p))
+                    self.labels.append(mask * 6 + gender * 3 + age)
+                    
         else:
             self.df = pd.read_csv(os.path.join(root, 'eval', 'info.csv'))
             self.paths = [os.path.join(root, 'eval', 'images', img_id) for img_id in self.df.ImageID]
@@ -66,7 +75,7 @@ class MaskDataLoader(BaseDataLoader):
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
-            # transforms.CenterCrop((192, 256))
+            transforms.Resize((192, 256))
         ])
         # self.data_dir = data_dir
         # self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
